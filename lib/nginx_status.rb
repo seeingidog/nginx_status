ab1a3374d72e527268c4cb5e9b1d7a4b21e6a559
seeingidog <ian@ruby-code.com>
2011-06-23 00:07:05 -0400

    separated parser, added test

--- lib/nginx_status.rb
+++ lib/nginx_status.rb
@@ -1,12 +1,18 @@
+require 'rubygems'
 require 'httparty'
 
-# Read Nginx status and return a Hash of current attributes
+# Read Nginx status and return a Hash of current status counters
 
 class NginxStatus
   include HTTParty
     
   def self.get_status(host)
-    status = get("http://#{host}/nginx_status").parsed_response.split("\n")
+    response = get("http://#{host}/nginx_status").parsed_response
+    parse_status(response)
+  end
+  
+  def self.parse_status(parsed_response)
+    status = parsed_response.split("\n")
     rww = status[3].split
     sahr = status[2].split
     {:active_connections => status[0].gsub("Active connections: ", "").to_i, :accepted => sahr[0].to_i, :handled => sahr[1].to_i, :handles => sahr[2].to_i, :reading => rww[1].to_i, :writing => rww[3].to_i, :waiting => rww[5].to_i}
--- spec/nginx_status_spec.rb
+++ spec/nginx_status_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+MOCK_RESPONSE = "Active connections: 183
+server accepts handled requests
+ 914384 914384 2725561
+Reading: 3 Writing: 2 Waiting: 178
+"
+
 describe "NginxStatus" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "parses mock /nginx_status response" do
+    expected = {:waiting=>178, :handled=>914384, :reading=>3, :active_connections=>183, :accepted=>914384, :handles=>2725561, :writing=>2}
+    NginxStatus.parse_status(MOCK_RESPONSE).should == expected
   end
+  
 end
