653eb5538feff674f397b64f2c2e383b66d35173
Ian Morgan <ian@ruby-code.com>
2011-07-01 11:20:20 -0400

    more tests

--- spec/nginx_status_spec.rb
+++ spec/nginx_status_spec.rb
@@ -8,7 +8,16 @@ Reading: 3 Writing: 2 Waiting: 178
 MOCK_INCORRECT_RESPONSE = "error"
 
 describe "NginxStatus" do
-  it "parses mock /nginx_status response" do
+  it "parses mock /nginx_status response and returns a Hash" do
+    NginxStatus.parse_status(MOCK_CORRECT_RESPONSE).class.should == Hash
+  end
+  
+  it "parses mock /nginx_status response and returns a Hash with the correct keys" do
+    expected_keys = [:active_connections, :accepted, :handled, :handles, :reading, :writing, :waiting]
+    NginxStatus.parse_status(MOCK_CORRECT_RESPONSE).keys.should == expected_keys
+  end
+  
+  it "parses mock /nginx_status response and returns the correct Hash" do
     expected = {:waiting=>178, :handled=>914384, :reading=>3, :active_connections=>183, :accepted=>914384, :handles=>2725561, :writing=>2}
     NginxStatus.parse_status(MOCK_CORRECT_RESPONSE).should == expected
   end
